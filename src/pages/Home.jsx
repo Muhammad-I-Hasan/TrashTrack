2047e9dbe60dc722827122d7ea4c19f188857c78
jaimie.marchuk <jaimie.marchuk@gmail.com>
2025-03-12 10:30:01 -0700

    Added design for home pg

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,70 @@
-import { Button } from "@mui/material";
+import React from "react";
+import { Button, Box, Typography, IconButton } from "@mui/material";
+import { Link } from "react-router-dom";
+import SettingsIcon from "@mui/icons-material/Settings";
+import "../App.css";
+import logo from "../images/Trash-Track-Logo1.png";
 
 export default function Home() {
-    return (
-      <div>
-        <h1>Home Page</h1>
-        <Button variant="contained">Click Me</Button>
+  return (
+    <div className="page" style={{ display: "flex", flexDirection: "column", height: "100vh", overflow: "hidden" }}>
+      {/* Top Bar with Settings Button */}
+      <Box sx={{ position: "absolute", top: 30, right: 10 }}>
+        <Link to="/settings">
+          <IconButton color="primary" aria-label="settings" sx={{ fontSize: "3rem" }}>
+            <SettingsIcon fontSize="inherit" />
+          </IconButton>
+        </Link>
+      </Box>
+
+      {/* Main Content Area */}
+      <div className="content" style={{ display: "flex", flexDirection: "column", flex: 1, alignItems: "center", justifyContent: "center", padding: "20px" }}>
+        {/* <Typography variant="h3" sx={{ fontWeight: "bold", color: "#333", marginBottom: "20px" }}>
+          Welcome to TrashTrack
+        </Typography> */}
+
+        {/* Logo Display */}
+        <Box
+          sx={{
+            marginBottom: "20px",
+            width: "350px",
+            height: "350px",
+            backgroundImage: `url(${logo})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            borderRadius: "50%",
+          }}
+        />
+
+        {/* Navigation Buttons */}
+        <Box sx={{ display: "flex", flexDirection: "column", gap: "15px", width: "100%", maxWidth: "400px", marginTop: "-20px" }}>
+          <Link to="/page1" style={{ textDecoration: "none" }}>
+            <Button variant="contained" color="primary" fullWidth sx={{ height: "60px", fontSize: "18px", textTransform: "none" }}>
+              Waste Scanner
+            </Button>
+          </Link>
+          <Link to="/page2" style={{ textDecoration: "none" }}>
+            <Button variant="contained" color="primary" fullWidth sx={{ height: "60px", fontSize: "18px", textTransform: "none" }}>
+              Waste Catalog
+            </Button>
+          </Link>
+          <Link to="/page3" style={{ textDecoration: "none" }}>
+            <Button variant="contained" color="primary" fullWidth sx={{ height: "60px", fontSize: "18px", textTransform: "none" }}>
+              Trash Schedule
+            </Button>
+          </Link>
+          <Link to="/page4" style={{ textDecoration: "none" }}>
+            <Button variant="contained" color="primary" fullWidth sx={{ height: "60px", fontSize: "18px", textTransform: "none" }}>
+              Locate Depot
+            </Button>
+          </Link>
+          <Link to="/page5" style={{ textDecoration: "none" }}>
+            <Button variant="contained" color="primary" fullWidth sx={{ height: "60px", fontSize: "18px", textTransform: "none" }}>
+              Create Waste Labels
+            </Button>
+          </Link>
+        </Box>
       </div>
-    );
-  }
+    </div>
+  );
+}
